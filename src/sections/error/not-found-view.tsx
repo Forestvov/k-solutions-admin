5949c9128e751d15b0e6404269ea99421474a36e
forestvov1 <kukva2019@gmail.com>
2024-04-21 20:03:58 +1000

    init admin

---
+++ src/sections/error/not-found-view.tsx
@@ -0,0 +1,43 @@
+import { m } from 'framer-motion';
+
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import { RouterLink } from 'src/routes/components';
+
+import { PageNotFoundIllustration } from 'src/assets/illustrations';
+
+import { varBounce, MotionContainer } from 'src/components/animate';
+
+// ----------------------------------------------------------------------
+
+export default function NotFoundView() {
+  return (
+    <MotionContainer>
+      <m.div variants={varBounce().in}>
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          Страница не найдена !
+        </Typography>
+      </m.div>
+
+      <m.div variants={varBounce().in}>
+        <Typography sx={{ color: 'text.secondary' }}>
+          К сожалению, мы не смогли найти страницу, которую вы ищете. Возможно, вы ошиблись в URL-адресе?
+        </Typography>
+      </m.div>
+
+      <m.div variants={varBounce().in}>
+        <PageNotFoundIllustration
+          sx={{
+            height: 260,
+            my: { xs: 5, sm: 10 },
+          }}
+        />
+      </m.div>
+
+      <Button component={RouterLink} href="/" size="large" variant="contained">
+        На главную
+      </Button>
+    </MotionContainer>
+  );
+}
