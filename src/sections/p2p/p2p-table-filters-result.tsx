82efb427ef915cd6c1645027db3d2e73f45f42d3
forestvov1 <kukva2019@gmail.com>
2024-05-01 14:31:29 +1000

    fix

--- src/sections/p2p/p2p-table-filters-result.tsx
+++ src/sections/p2p/p2p-table-filters-result.tsx
@@ -67,34 +67,34 @@ export default function P2pTableFiltersResult({
 }
 
 // ----------------------------------------------------------------------
-
-type BlockProps = StackProps & {
-  label: string;
-};
-
-function Block({ label, children, sx, ...other }: BlockProps) {
-  return (
-    <Stack
-      component={Paper}
-      variant="outlined"
-      spacing={1}
-      direction="row"
-      sx={{
-        p: 1,
-        borderRadius: 1,
-        overflow: 'hidden',
-        borderStyle: 'dashed',
-        ...sx,
-      }}
-      {...other}
-    >
-      <Box component="span" sx={{ typography: 'subtitle2' }}>
-        {label}
-      </Box>
-
-      <Stack spacing={1} direction="row" flexWrap="wrap">
-        {children}
-      </Stack>
-    </Stack>
-  );
-}
+//
+// type BlockProps = StackProps & {
+//   label: string;
+// };
+//
+// function Block({ label, children, sx, ...other }: BlockProps) {
+//   return (
+//     <Stack
+//       component={Paper}
+//       variant="outlined"
+//       spacing={1}
+//       direction="row"
+//       sx={{
+//         p: 1,
+//         borderRadius: 1,
+//         overflow: 'hidden',
+//         borderStyle: 'dashed',
+//         ...sx,
+//       }}
+//       {...other}
+//     >
+//       <Box component="span" sx={{ typography: 'subtitle2' }}>
+//         {label}
+//       </Box>
+//
+//       <Stack spacing={1} direction="row" flexWrap="wrap">
+//         {children}
+//       </Stack>
+//     </Stack>
+//   );
+// }
