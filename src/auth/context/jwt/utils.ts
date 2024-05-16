842114084d7ddc7c105ec9795278f11e32211ad5
forestvov1 <kukva2019@gmail.com>
2024-05-17 01:09:34 +1000

    fix

--- src/auth/context/jwt/utils.ts
+++ src/auth/context/jwt/utils.ts
@@ -5,5 +5,9 @@ export const setSession = (acceptToken: string | null, refreshToken: string | nu
     localStorage.setItem('acceptToken', acceptToken);
     localStorage.setItem('refreshToken', refreshToken);
     axios.defaults.headers.common.Authorization = acceptToken;
+  } else {
+    localStorage.removeItem('acceptToken');
+    localStorage.removeItem('refreshToken');
+    axios.defaults.headers.common.Authorization = null;
   }
 };
